1dcfbd82d07b32d546ed2cc4e52bcd34705ee79b
Pavan699 <pavannakate03@gmail.com>
2021-10-16 23:43:14 -0700

    Adding Title for ADO.Net Project

---
+++ ADOEmpRollPay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADOEmpRollPay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to ADO.Net Program");
+        }
+    }
+}
